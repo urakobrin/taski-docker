bbb2769c4f487a69eb6ac1601dac211e59b68b95
Комарчук Юрий <komarchuk57@mail.ru>
2023-09-03 18:41:58 +0300

    settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,8 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['158.160.10.241', '127.0.0.1', 'localhost', 'nifnyfnaf.hopto.org']
+ALLOWED_HOSTS = ['158.160.10.241', '127.0.0.1',
+                 'localhost', 'nifnyfnaf.hopto.org']
 
 
 # Application definition
@@ -123,7 +124,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000'
 ]
-
-TELEGRAM_TO = 885496893
-
-TELEGRAM_TOKEN = 6149390048:AAH03mrEOCSMf6JmbI2-UsOuAWk6_oeDihY
